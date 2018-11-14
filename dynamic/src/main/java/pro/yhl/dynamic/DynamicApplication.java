fbe6009760fce11011e6f707105220f04d451008
yuanhailiang <yuanhailiang@ebupt.com>
2018-11-14 19:23:01 +0800

    初始化动态模块

---
+++ dynamic/src/main/java/pro/yhl/dynamic/DynamicApplication.java
@@ -0,0 +1,12 @@
+package pro.yhl.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicApplication.class, args);
+    }
+}
