81e4cf24b87dd77ca959f2ed7a8c900643bb1aae
yuanhailiang <yuanhailiang@ebupt.com>
2018-11-14 19:38:14 +0800

    初始化 配置服务器 模块

---
+++ config/src/main/java/pro/yhl/config/ConfigApplication.java
@@ -0,0 +1,12 @@
+package pro.yhl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
